e501199ad8c6d5b5f2ac0805f8f0edd2098b1552
DidyMas <64103758+DidyMas@users.noreply.github.com>
2021-04-08 20:33:22 -0500

    Ajutes Body

--- Index.php
+++ Index.php
@@ -49,13 +49,13 @@ $pagina = obtenerPaginaActual();
         <div class="navbar-collapse collapse d-sm-inline-flex flex-sm-row-reverse">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link NSact">Alta</a>
+              <a class="nav-link NSact"></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link NSact">Data</a>
+              <a class="nav-link NSact"></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link NSact">Archivo</a>
+              <a class="nav-link NSact"></a>
             </li>
           </ul>
         </div>
@@ -81,13 +81,161 @@ $pagina = obtenerPaginaActual();
     <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
 </div>
 <div class="col-md-12" style="height:50px;"></div>
-<section class="pt-12 pt-md-13 bg-gray-200">
+      <div class="row1">
+        <!-- Content -->
+        <div class="col-sm-6">
+                <h1 class="display-4">
+                La atención medica en línea es el 
+                  <span class="text-primary text-highlight-warning">
+                    <span class="js-text-animation">futuro</span><span class="typed-cursor typed-cursor--blink">!</span>
+                  </span>
+                </h1>
+                <p class="lead">Conoce a nuestro equipo médico y recibe la atención especializada que te mereces.</p>
+
+              <a class="btn btn-primary btn-wide transition-3d-hover" href="platform/login.php?user=doctor">Crear una cuenta</a>
+              <a class="btn btn-link btn-wide" href="#">Lee más!<i class="fas fa-angle-right fa-sm ml-1"></i></a>
+        </div>
+        <!-- End Content -->
+        <!-- img Shape -->
+        <figure class="col-sm-6">
+            <g >
+              <image class="mx-auto d-block imgfluid" src="platform/img/Sactdocfble.jpg" ></image>
+            </g>
+          </svg>
+        </figure>
+        <!-- End img Shape -->
+      </div>
+      <div class="col-md-12" style="height:50px;"></div>
+      <section class="pt-12 SacdocF">
       <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-md-10 col-lg-7 text-center">
+            
+            <!-- Heading -->
+            <h2 class="sactext">
+            Lorem ipsum dolor sit amet consectetur
+            </h2>
+
+            <!-- Text -->
+            <p class="sactexta">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit porttitor aptent, justo cursus netus purus blandit convallis massa ut ac, himenaeos imperdiet lectus enim class consequat risus nam.
+            </p>
+
+          </div>
+        </div> <!-- / .row -->
+        <div class="form-row">
+          <div class="col-12 col-lg-6 d-lg-flex pb-1 mb-4">
+            
+            <!-- Card -->
+            <div class="card" data-aos="fade-up">
+              <div class="row">
+                <div class="col-md-4 position-relative">
+                  
+                  <!-- Image -->
+                  <img src="platform/img/SactivaDocdig.jpg" class="mx-auto d-block img-fluid" alt="...">
+
+                </div>
+                <div class="col-md-8">
+                  
+                  <!-- Body -->
+                  <div class="card-body py-7 py-md-9 text-center">
+                    
+                    <!-- Heading -->
+                    <h4 class="font-weight-bold">
+                    Lorem ipsum dolor
+                    </h4>
+
+                    <!-- Text -->
+                    <p class="text-muted mb-0">
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit porttitor aptent, justo cursus netus purus blandit convallis massa ut ac, himenaeos imperdiet lectus enim class consequat risus nam.
+                    </p>
+                    
+                  </div>
+
+                </div>
+              </div> <!-- / .row -->
+            </div>
+          </div>
+          <div class="col-12 col-lg-6 d-lg-flex pb-1 mb-4">
+            
+            <!-- Card -->
+            <div class="card shadow-light-lg overflow-hidden text-center aos-init aos-animate" data-aos="fade-up">
+              <div class="row">
+                <div class="col-md-8">
+                  
+                  <!-- Body -->
+                  <div class="card-body py-7 py-md-9">
+                    
+                    <!-- Heading -->
+                    <h4 class="font-weight-bold">
+                    Lorem ipsum dolor
+                    </h4>
+
+                    <!-- Text -->
+                    <p class="text-muted mb-0">
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit porttitor aptent, justo cursus netus purus blandit convallis massa ut ac, himenaeos imperdiet lectus enim class consequat risus nam.
+                    </p>
+                    
+                  </div>
+
+                </div>
+                <div class="col-md-4">
+                  
+                  <!-- Image -->
+                  <img src="platform/img/SactivaDocdig1.jpg" class="mx-auto d-block img-fluid left-0" alt="...">
+
+                </div>
+              </div> <!-- / .row -->
+            </div>
+
+          </div>
+        </div> <!-- / .row -->
+      </div> <!-- / .container -->
+    </section>
+<div class="col-md-12" style="height:50px;"></div>
+<div class="row">
+          <div class="col-12">
+            
+            <!-- Card -->
+            <div class="card shadow-light-lg overflow-hidden text-center text-lg-left aos-init aos-animate" data-aos="fade-up">
+              <div class="row">
+                <div class="col-md-4 position-relative">
+                  
+                  <!-- Image -->
+                  <img src="platform/img/SactivaDocDig3.jpg" class="mx-auto d-block img-fluid" alt="...">
+
+                </div>
+                <div class="col-md-8">
+
+                  <!-- Body -->
+                  <div class="card-body py-7 py-md-9">
+                  
+                    <!-- Heading -->
+                    <h4 class="font-weight-bold">
+                    Lorem ipsum dolor
+                    </h4>
+
+                    <!-- Text -->
+                    <p class="text-muted mb-0">
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit porttitor aptent, justo cursus netus purus blandit convallis massa ut ac, himenaeos imperdiet lectus enim class consequat risus nam.
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit porttitor aptent, justo cursus netus purus blandit convallis massa ut ac, himenaeos imperdiet lectus enim class consequat risus nam.
+                    </p>
+
+                  </div>
+
+                </div>
+              </div> <!-- / .row -->
+            </div>
+          </div>
+        </div> <!-- / .row -->
+        <div class="col-md-12" style="height:50px;"></div>
+<section class="pt-12">
+    <div class="container-fluid">
         <div class="row align-items-center">
           <div class="col-12 col-md-5 col-lg-6 order-md-2">
 
             <!-- Image -->
-            <img src="Content/stethoscope.jpg" alt="..." class="img-fluid mb-6 mb-md-0">
+            <img src="Content/stethoscope.jpg" alt="..." class="img-fluid mb-6 mb-md-0" style="border-radius: 20px;">
 
           </div>
           <div class="col-12 col-md-7 col-lg-6 order-md-1">
@@ -137,7 +285,7 @@ $pagina = obtenerPaginaActual();
           </div>
         </div> <!-- / .row -->
       </div>
- <div class="col-md-12" style="height:50px;"></div>
+      <div class="col-md-12" style="height:50px;"></div>
 </main>
 
 </div>
